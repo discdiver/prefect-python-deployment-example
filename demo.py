38f134058a90c96db5a6a625b0518659f455e954
Jeff Hale <jeffmshale@gmail.com>
2022-08-31 18:07:46 -0400

    remove extra

--- demo.py
+++ demo.py
@@ -1,20 +1,23 @@
-import pandas
 import yfinance as yf
 from datetime import timedelta
 from prefect import flow, task
 
+
 @task(retries=3, cache_expiration=timedelta(30))
 def fetch_data(ticker):
     return yf.download(ticker)
-    
+
+
 @task
 def save_data(stock_df):
-    stock_df.to_parquet()
+    stock_df.to_csv()
+
 
 @flow
 def pipeline(ticker):
     df = fetch_data(ticker)
     save_data(df)
-    
+
+
 if __name__ == "__main__":
     pipeline("AAPL")
